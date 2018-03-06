a3cdfc9bfd7428fd286fd67774d35a8e6521f981
lazycesar <cesarespi36@hotmail.com>
2018-03-06 19:34:11 +0100

    Add js and dropdown menu

---
+++ assets/style.js
@@ -0,0 +1,31 @@
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("product-dropdown").classList.toggle("show"); 
+}
+function myFunction2() {
+  document.getElementById("resources-dropdown").classList.toggle("show"); 
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("header-dropdown");
+    for (var i = 0;dropdowns[i]; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+  // else if (!event.target.matches('.dropbtn2')) {
+  //   var dropdowns = document.getElementsByClassName("header-dropdown");
+  //   var i;
+  //   for (i = 0; i < dropdowns.length; i++) {
+  //     var openDropdown = dropdowns[i];
+  //     if (openDropdown.classList.contains('show')) {
+  //       openDropdown.classList.remove('show');
+  //     }
+  //   }
+  // }
+}
+
+
